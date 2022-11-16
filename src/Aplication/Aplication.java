a5e596b2a426377ab3df0e06d8b276e19b0b1ef0
Albi <albimelgarlopez@gmail.com>
2022-11-16 18:46:02 +0100

    1ra version

---
+++ src/Aplication/Aplication.java
@@ -0,0 +1,23 @@
+package Aplication;
+
+public class Aplication {
+    public static void main(String[] args) {
+        String nombreApellido;
+        int modulosMatriculados;
+        int horasDedicadas;
+
+        System.out.println(args[0]);
+        System.out.println(veredicto(args[1],args[2]));
+    }
+
+    static String veredicto(String valor1, String valor2) {
+
+        if (valor1.equals(valor2)) {
+            return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+        }
+        if (Integer.valueOf(valor1) < Integer.valueOf(valor2)) {
+            return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+        }
+        return "Ideal. Trabajas los contenidos en casa.";
+    }
+}
